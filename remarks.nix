9bd7a508f9e28d804792b1ef3061a9a57bd47e8d
Laura Brekelmans <mail@laura.fm>
2024-09-25 19:08:29 +0200

    Improve nix remarks environment

--- remarks.nix
+++ remarks.nix
@@ -1,4 +1,20 @@
 with import <nixpkgs> {};
+let
+  shellHook = pkgs.writeShellApplication {
+    name = "shellhook";
+    text = ''
+      export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+
+      if ! [[ -d .venv ]]; then
+        poetry env use python3.10
+        poetry install
+      else
+        # shellcheck disable=SC1091
+        source ./.venv/bin/activate
+      fi
+    '';
+  };
+in
 pkgs.mkShell {
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc zlib ];
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
@@ -10,9 +26,6 @@ pkgs.mkShell {
     pkgs.gnumake
   ];
 
-  shellHooks = ''
-    export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-    source ./venv/bin/activate
-  '';
+  shellHooks = "${shellHook}/bin/shellhook";
   
 }
