4a2078edb1603ac4f170df958db9ecc0d062883f
Laura Brekelmans <mail@laura.fm>
2024-09-25 19:16:23 +0200

    Fix sourcing in the nix script

--- remarks.nix
+++ remarks.nix
@@ -1,19 +1,26 @@
 with import <nixpkgs> {};
 let
-  shellHook = pkgs.writeShellApplication {
-    name = "shellhook";
-    text = ''
-      export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+  shellHookScript = ''
+    #!/usr/bin/bash
+    export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
 
-      if ! [[ -d .venv ]]; then
-        poetry env use python3.10
-        poetry install
-      else
-        # shellcheck disable=SC1091
-        source ./.venv/bin/activate
-      fi
-    '';
-  };
+    if ! [[ -d .venv ]]; then
+      poetry env use python3.10
+      poetry install
+    else
+      # shellcheck disable=SC1091
+      source ./.venv/bin/activate
+    fi
+  '';
+
+  shellHookChecked = pkgs.runCommand "shell-hook-checked" {
+    buildInputs = [ pkgs.shellcheck ];
+  } ''
+    echo "${shellHookScript}" > shellhook.sh
+    shellcheck shellhook.sh
+    mkdir -p $out
+    cp shellhook.sh $out/
+  '';
 in
 pkgs.mkShell {
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc zlib ];
@@ -26,6 +33,5 @@ pkgs.mkShell {
     pkgs.gnumake
   ];
 
-  shellHooks = "${shellHook}/bin/shellhook";
-  
+  shellHook = builtins.readFile "${shellHookChecked}/shellhook.sh";
 }
